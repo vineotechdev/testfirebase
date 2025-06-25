5ec728f5a4fa7647104694c2e011dab3cf24c784
vneotechdev <vineotechdev@gmail.com>
2025-06-25 20:16:18 +1000

    ok

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,83 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { IonInput, IonButton, IonGrid, IonRow, IonCol, IonText } from '@ionic/react';
 
-const Tab1: React.FC = () => {
+type FormData = {
+  username: string;
+  password: string;
+};
+
+const SimpleLoginForm: React.FC = () => {
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log('Submitted:', data);
+    // login user with below details in firebase test@gmailtest.com password- test@gmailtest.com
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+    <div style={{ paddingTop: 48 }}> {/* Adds top padding for iOS */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <Controller
+                name="username"
+                control={control}
+                rules={{ required: 'Username (email) is required' }}
+                render={({ field }) => (
+                  <IonInput
+                    {...field}
+                    label="Email (use: test@gmailtest.com)"
+                    labelPlacement="floating"
+                    placeholder="Enter email (test@gmailtest.com)"
+                    fill="outline"
+                    type="email"
+                  />
+                )}
+              />
+              {errors.username && (
+                <IonText color="danger">
+                  <p>{errors.username.message}</p>
+                </IonText>
+              )}
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <Controller
+                name="password"
+                control={control}
+                rules={{ required: 'Password is required' }}
+                render={({ field }) => (
+                  <IonInput
+                    {...field}
+                    type="password"
+                    label="Password (use: test@gmailtest.com)"
+                    labelPlacement="floating"
+                    placeholder="Enter password (test@gmailtest.com)"
+                    fill="outline"
+                  />
+                )}
+              />
+              {errors.password && (
+                <IonText color="danger">
+                  <p>{errors.password.message}</p>
+                </IonText>
+              )}
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton expand="block" type="submit">
+                Login
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </form>
+    </div>
   );
 };
 
-export default Tab1;
+export default SimpleLoginForm;
